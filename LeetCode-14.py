364454d8a61322e69f6c8b826f4710cecdce5aaa
Bino <binonguyen1090@gmail.com>
2019-11-03 11:05:09 -0600

    Update

--- LeetCode-14.py
+++ LeetCode-14.py
@@ -3,32 +3,47 @@
 class Solution:
     def longestCommonPrefix(self, strs: List[str]) -> str:
         result = ""
-        if len(strs) == 'null' or len(strs) == 0:
-            return(result)
-        elif len(strs) == 1:
-            return strs[0]
-        else:
-            l = len(strs[0])
-            shorted = ""
-            for i in range(0,len(strs)):
-              if len(strs[i]) <= l:
-                l = len(strs[i])
-                shorted = strs[i]
+        if (len(strs) == 0):
+           return (result)
+
+        for i in range (0, len(strs[0])):
+          chara = strs[0][i]
+          for k in range (1,len(strs)):
+          
+            if (i == len(strs[k]) or strs[k][i] != chara):
+                return(result)
+          result += chara
+        return (result)
+
+# class Solution:
+#     def longestCommonPrefix(self, strs: List[str]) -> str:
+#         result = ""
+#         if len(strs) == 'null' or len(strs) == 0:
+#             return(result)
+#         elif len(strs) == 1:
+#             return strs[0]
+#         else:
+#             l = len(strs[0])
+#             shorted = ""
+#             for i in range(0,len(strs)):
+#               if len(strs[i]) <= l:
+#                 l = len(strs[i])
+#                 shorted = strs[i]
 
             
-            for j in range(0,len(shorted)):
+#             for j in range(0,len(shorted)):
 
-              if check(shorted[j],strs,j):
-                result+=(shorted[j])
-              else:
-                return (result)
+#               if check(shorted[j],strs,j):
+#                 result+=(shorted[j])
+#               else:
+#                 return (result)
 
-            print(result)
-            return(result)
+#             print(result)
+#             return(result)
 
     
-def check(s,a,i):
-  for word in a:
-    if s != word[i]:
-      return False
-  return True
+# def check(s,a,i):
+#   for word in a:
+#     if s != word[i]:
+#       return False
+#   return True
