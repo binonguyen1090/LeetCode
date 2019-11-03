0d7033914c4386041026cdded5e6ea539f1e98f8
Bino <binonguyen1090@gmail.com>
2019-11-03 14:16:03 -0600

    LeetCode

---
+++ LeetCode-26.py
@@ -0,0 +1,11 @@
+# https://leetcode.com/problems/remove-duplicates-from-sorted-array/submissions/
+class Solution:
+    def removeDuplicates(self, nums: List[int]) -> int:
+        index = 1
+        
+        while index < len(nums):
+            if nums[index] == nums[index-1]:
+                nums.remove(nums[index])
+            else:
+                index += 1
+        return(len(nums))
