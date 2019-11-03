cc0273cd4470c31fdf7a2e06109faeaa9d0c6d30
Bino <binonguyen1090@gmail.com>
2019-11-02 22:11:37 -0500

    LeetCode

---
+++ LeetCode-14.py
@@ -0,0 +1,34 @@
+#https://leetcode.com/problems/longest-common-prefix/
+
+class Solution:
+    def longestCommonPrefix(self, strs: List[str]) -> str:
+        result = ""
+        if len(strs) == 'null' or len(strs) == 0:
+            return(result)
+        elif len(strs) == 1:
+            return strs[0]
+        else:
+            l = len(strs[0])
+            shorted = ""
+            for i in range(0,len(strs)):
+              if len(strs[i]) <= l:
+                l = len(strs[i])
+                shorted = strs[i]
+
+            
+            for j in range(0,len(shorted)):
+
+              if check(shorted[j],strs,j):
+                result+=(shorted[j])
+              else:
+                return (result)
+
+            print(result)
+            return(result)
+
+    
+def check(s,a,i):
+  for word in a:
+    if s != word[i]:
+      return False
+  return True
