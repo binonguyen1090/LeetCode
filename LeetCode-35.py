2447eaf7f297e10e71295d926b573b3962de7f0e
Bino <binonguyen1090@gmail.com>
2019-11-03 21:15:28 -0600

    LeetCode

---
+++ LeetCode-35.py
@@ -0,0 +1,15 @@
+
+# https://leetcode.com/problems/search-insert-position/
+class Solution:
+    def searchInsert(self, nums: List[int], target: int) -> int:
+        for i in range(0,len(nums)):
+            if nums[i] == target:
+                return(i)
+            else:
+                nums.append(target)
+        
+        
+        nums.sort()
+        for k in range (0,len(nums)):
+            if nums[k] == target:
+                return(k)
