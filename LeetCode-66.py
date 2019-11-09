afcd59ae1a414aca8b19da60a0abe520d4cb0d20
Bino <binonguyen1090@gmail.com>
2019-11-09 17:50:54 -0600

    update

--- LeetCode-66.py
+++ LeetCode-66.py
@@ -1,10 +1,22 @@
 #https://leetcode.com/problems/plus-one/submissions/
+# class Solution:
+#     def plusOne(self, digits: List[int]) -> List[int]:
+#         total = 0
+#         for i in digits:
+#             total = total*10 + i
+#         s = str(total+1)
+#         result = list(s)
+#         return(result)
+            
+
+
 class Solution:
     def plusOne(self, digits: List[int]) -> List[int]:
-        total = 0
-        for i in digits:
-            total = total*10 + i
-        s = str(total+1)
-        result = list(s)
-        return(result)
-            
+        if digits[-1] == 9:
+            if len(digits) == 1:
+                return([1,0])
+            else:
+                
+                return self.plusOne(digits[:-1]) + [0]
+        digits[-1] = digits[-1]+ 1
+        return(digits)
