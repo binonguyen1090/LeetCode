4ef7ac4aecfa1042cbe02b1a3b398a4dd19a93ca
Bino <binonguyen1090@gmail.com>
2019-11-09 15:20:50 -0600

    update

--- LeetCode-58.py
+++ LeetCode-58.py
@@ -1,15 +1,21 @@
 #https://leetcode.com/problems/length-of-last-word/submissions/
+# class Solution:
+#     def lengthOfLastWord(self, s: str) -> int:
+        
+#         local_count = 0
+#         result = 0
+#         for i in range(0,len(s)):
+#             if s[i] == ' ':
+#                 local_count = 0
+#             else:
+#                 local_count += 1
+#                 result = local_count
+#         return(result)
+            
 class Solution:
     def lengthOfLastWord(self, s: str) -> int:
-        
-        local_count = 0
-        result = 0
-        for i in range(0,len(s)):
-            if s[i] == ' ':
-                local_count = 0
-            else:
-                local_count += 1
-                result = local_count
-        return(result)
+        while s.endswith(' '):
+            s = s[0:-1]
+        arr = s.split(' ')
+        return (len(arr[-1]))
             
-        
